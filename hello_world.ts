a9e455f6aa431bd1c804f012aed260028f3d2897
Abhishek <dhanani9638a@gmail.com>
2021-09-11 12:09:47 +0530

    define variable

---
+++ hello_world.ts
@@ -0,0 +1,23 @@
+console.log("Hello, world");
+
+{
+    let myNum: number = 1;
+    console.log("my Num = " + myNum);
+
+    let myString: string = "Hello, world";
+    console.log("My String = " + myString);
+
+    let myBoolean: boolean = true;
+    console.log("My Boolean = " + myBoolean);
+
+    let myStringArray: string[] = ["A", "B", "C", "D", "E"];
+    console.log("My String Array = " + myStringArray);
+
+    let myNumberArray: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    console.log("My Number Array = " + myNumberArray);
+
+    let anyTypeArray : any[] = [1, "ABC", true];
+    console.log("Any Type Array" + anyTypeArray);
+
+
+}
